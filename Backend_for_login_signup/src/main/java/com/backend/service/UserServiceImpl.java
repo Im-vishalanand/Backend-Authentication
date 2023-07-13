b5675578275fa6504314e6e09e03eca3f25c7fa3
Kumar Vishal Anand <vishalanand270@gmail.com>
2023-07-14 01:04:11 +0530

    backend applicatio  login and signup REST API with security features done

--- Backend_for_login_signup/src/main/java/com/backend/service/UserServiceImpl.java
+++ Backend_for_login_signup/src/main/java/com/backend/service/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.backend.dto.UserDTO;
